d019a64a5369cdcc8f05d12e2deb19bda6e6851f
sneha-lenka <63461983+sneha-lenka@users.noreply.github.com>
2020-05-05 19:55:58 +0530

    Add files via upload

---
+++ prod.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b;
+	cout<<"Enter two numbers";
+	cin>>a>>b;
+	cout<<"product="<<a*b;
+	return 0;
+}
---
+++ size.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"\n Size of char :"<<sizeof(char);
+	cout<<"\n Size of int :"<<sizeof(int);
+	cout<<"\n Size of float :"<<sizeof(float);
+	cout<<"\n Size of double :"<<sizeof(double);
+	return 0;
+}
